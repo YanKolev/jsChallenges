39336082e4d698c013444a41634089ca5e400e94
Yan Kolev <y.kolev11@gmail.com>
2024-02-01 16:24:56 +0200

    script update

---
+++ script.js
@@ -0,0 +1,20 @@
+//Variable and Values exercise
+let homeCountry = "Bulgaria";
+let originContinent = "Europe";
+let countryPopulation = "Six Million";
+console.log(homeCountry);
+console.log(originContinent);
+console.log(countryPopulation);
+
+//Two main datatypes in JS: Object or Primitive value
+/* 7 different types:
+1-Number: Floating point numbers- used for integers and floats
+2-String: Sequence of charaters- used for text
+3-Boolean: logical type true or false
+4-Undefined- value taken by a variable that is not yet defined(empty value)
+5-Null-Also means empty value
+6-Symbol (ES2015): value that is unique and can not be change
+7-BigInt (ES2020): larger integers the Nymber type can hold */
+
+//When you declare a variable an use let (let javascriptIsFun = true,)
+//you can assign new value to the already existing variable you can write it: javascriptIsFun = "Yes!"
