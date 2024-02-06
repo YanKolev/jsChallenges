277a69c6f732c0a06bedf2927447d53c259a37a3
Yan Kolev <y.kolev11@gmail.com>
2024-02-06 08:17:05 +0200

    second challenge

---
+++ challenge2.js
@@ -0,0 +1,22 @@
+/* Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement */
+
+//Previous code: 
+const markMasskg = 78;
+const markheightcentimeter = 169;
+const markheightmeter = markheightcentimeter/ 100;
+const markBMI = markMasskg / (markheightmeter * markheightmeter);
+
+const johnMasskg = 92;
+const johnheightcentimeter = 195;
+const johnheightmeter = johnheightcentimeter/ 100;
+const johnBMI =johnMasskg /(johnheightmeter * johnheightmeter);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
--- script.js
+++ script.js
@@ -49,3 +49,13 @@ if (age >=18){
     const yearsLeft = 18 - age;
     console.log(`Sarah is too young, wait another ${yearsLeft} years :)`)
 }
+
+const birthYear = 1991;
+
+let century;
+if(birthYear <= 2000){
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
