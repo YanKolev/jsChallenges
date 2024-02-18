cea5d0456c84c6aadaee613d9858233262ee57de
Yan Kolev <y.kolev11@gmail.com>
2024-02-18 17:00:11 +0200

    solution challenge7

--- challenge7.js
+++ challenge7.js
@@ -14,7 +14,7 @@ Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 tall.
 */
 
-
+/* ------MY method-----
 const john = {
     fullName: 'John Smith',
     heigth: 1.95, 
@@ -37,4 +37,38 @@ const mark = {
     }
 }
 
-console.log(mark.calcBMI());
+console.log(mark.calcBMI(), john.calcBMI());
+*/
+
+// ----- Course Method--------
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height:1.95,
+    calcBMI: function(){
+        this.bmi = this.mass /this.height ** 2;
+        return this.bmi;
+    }
+};
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height:1.69,
+    calcBMI: function(){
+        this.bmi = this.mass /this.height ** 2;
+        return this.bmi;
+    }
+};
+
+mark.calcBMI(); //invocing the function in order to print it
+john.calcBMI();
+
+console.log(mark.bmi, mark.bmi);
+
+if (mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+} else if (john.bmi > mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
