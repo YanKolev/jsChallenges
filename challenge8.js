bebc4594c0ef0d1a84a23a2e9bffd355de78009b
Yan Kolev <y.kolev11@gmail.com>
2024-02-20 17:08:12 +0200

    Solution challenge8

--- challenge8.js
+++ challenge8.js
@@ -6,7 +6,9 @@ Your tasks:
 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
 tips and total values (bill + tip) for every bill value in the bills array. Use a for
 loop to perform the 10 calculations!
+
 Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
 Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
 tips and totals arrays 😉
 Bonus:
@@ -26,3 +28,31 @@ length of the array (because that's the number of elements)
 4.3.
 Call the function with the 'totals' array
 */
+
+bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+tips = [];
+totals = [];
+
+const calcTip = function(bill){
+    return bill >= 50 && bill <= 300 ? bill * 0.15:
+    bill * 0.2;
+}
+
+//for(let i = 0; i < bills.lenght; i++){
+//    tips.push(calcTip() + bills[i]);
+// }
+
+/*let tips = 0;
+for(let i = 0;i <bills.length;i++){
+    tips.push(calcTip(bills));
+}
+console.log(tips); */
+
+//Course solution
+
+for(let i=0; i < bills.length;i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip+bills[i]);
+}
+console.log(bills,tips, totals);
