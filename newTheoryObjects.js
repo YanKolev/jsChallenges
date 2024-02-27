0e98801d491f569a202df8455b6d394ad1bdca44
Yan Kolev <y.kolev11@gmail.com>
2024-02-27 15:24:47 +0200

    expanding new theory

---
+++ newTheoryObjects.js
@@ -0,0 +1,6 @@
+const owedMoney = {}; //creates empty object
+owedMoney ["Jimmy"] = 5; //we assign value of 5 to key jimmy
+owedMoney ["Anna"] = 7; // same for anna as for jimmy
+// if we call the key jimmy, like: owedMoney ["Jimmy"]; ->the result will be 5
+owedMoney ["Jien"] // it will return undeintified because we did not set such key-value pair in the first place.
+
