5a8cc6ba8db9d8b425041c2c03004509810d913b
Yan Kolev <y.kolev11@gmail.com>
2024-02-29 12:14:50 +0200

    optimized variables

--- hangman.js
+++ hangman.js
@@ -1,3 +1,36 @@
+/*
+Instructions on how to create the game: 
+
+1.Pick a random word.
+2. Take the player’s guess.
+3. Quit the game if the player wants to.
+4. Check that the player’s guess is a valid letter.
+5. Keep track of letters the player has guessed.
+6. Show the player their progress.
+7.Finish when the player has guessed the word.
+
+//Pseudocode: 
+
+Pick a random word
+While the word has not been guessed {
+Show the player their current progress
+Get a guess from the player
+If the player wants to quit the game {
+Quit the game
+}
+Else If the guess is not a single letter {
+Tell the player to pick a single letter
+}
+Else {
+If the guess is in the word {
+Update the player's progress with the guess
+}
+}
+}
+Congratulate the player on guessing the word
+
+*/
+
 // Create an array of words
 const words = [
     "javascript",
@@ -9,7 +42,7 @@ const words = [
 const word = words[Math.floor(Math.random() * words.length)];
     // Set up the answer array
 let answerArray = [];
-for (var i = 0; i < word.length; i++) {
+for (let i = 0; i < word.length; i++) {
 answerArray[i] = "_";
 }
 let remainingLetters = word.length;
@@ -26,7 +59,7 @@ break;
 alert("Please enter a single letter.");
 } else {
 // Update the game state with the guess
-for (var j = 0; j < word.length; j++) {
+for (let j = 0; j < word.length; j++) {
 if (word[j] === guess) {
 answerArray[j] = guess;
 remainingLetters--;
