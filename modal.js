170204c81b8b304321382fe9b723b8d40b43c498
Yan Kolev <y.kolev11@gmail.com>
2024-03-19 17:15:45 +0200

    adding functions for removing/adding clases

--- modal.js
+++ modal.js
@@ -6,7 +6,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 //Whenever we are using queryselector with a selector which matches multiple elements
 // only the first one will get selected. we need to put them in a nodelist with queryselectorAll
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = function(){
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+//making a function to use for closing the popup
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
 
 // for (let i = 0; i <btnsOpenModal.length; i++)
 // console.log(btnsOpenModal[i].textContent);
@@ -17,7 +29,10 @@ to the element. Being that eventhandler and event lister are almost the same
 we can use them interchangebly and make it so it can listens to the event 'click'. */
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-btnsOpenModal[i].addEventListener('click', function(){
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-});
+btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click',closeModal); //NB- do not call the function as JS will execute the code and will not work
+//we want the function closemodal only to be executed as the click event happens- hence no empty brackets after it.
+
+//making the popup close by click anywhere on the screen
+overlay.addEventListener('click', closeModal);
