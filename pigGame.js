1f1eae6e99dc96f1f0341de740edd73918a9b178
Yan Kolev <y.kolev11@gmail.com>
2024-03-22 18:16:10 +0200

    First update

--- pigGame.js
+++ pigGame.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting elements
+const score0Element = document.querySelector('#score--0'); // using # when we are using ID, and dot is for classes
+const score1Element = document.getElementById ('score--1'); // you can use getelementby ID
+const diceElement = document.querySelector('.dice');
+
+//starting status
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
