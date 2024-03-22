be35f7e803b3e9384d9b8d034bbcad3d5cb5410f
Yan Kolev <y.kolev11@gmail.com>
2024-03-22 20:29:47 +0200

    Update 4

--- pigGame.js
+++ pigGame.js
@@ -21,34 +21,71 @@ score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
-const score = [0, 0];
+const scores = [0, 0];
 let currentScore = 0; //can not be inside a function, otherwise it will be always set to zero, hence its outside
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0; //making the score back to zero before the switch with ternary operator below
+    activePlayer = activePlayer === 0 ? 1: 0; //allowing us to switch players(from player 0 to player 1 )
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
+};
 
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function(){
-    //1. generating random dice roll
-    const dice = Math.trunc(Math.random()* 6)+1;
-    console.log(dice);
+    if(playing){
 
-    //2.display dice
-    diceElement.classList.remove('hidden');
-    diceElement.src = `pics/dice-${dice}.png`;
+        //1. generating random dice roll
+        const dice = Math.trunc(Math.random()* 6)+1;
+        console.log(dice);
+
+        //2.display dice
+        diceElement.classList.remove('hidden');
+        diceElement.src = `pics/dice-${dice}.png`;
 
 
-    //3.Check for rolled 1, if true switch to next player
-    if (dice !== 1) {
-        //add dice to current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent =currentScore; // selecting the score element dynamically based on which is the active player right now
-    } else {
-        //Swith to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0; //making the score back to zero before the switch with ternary operator below
-        activePlayer = activePlayer === 0 ? 1: 0; //allowing us to switch players(from player 0 to player 1 )
-        currentScore = 0;
-        player0Element.classList.toggle('player--active');
-        player1Element.classList.toggle('player--active');
+        //3.Check for rolled 1, if true switch to next player
+        if (dice !== 1) {
+            //add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent =currentScore; // selecting the score element dynamically based on which is the active player right now
+        } else {
+            //Swith to next player
+            switchPlayer();
 
+
+        }
+    }    
+});
+
+btnHold.addEventListener('click', function(){
+    if(playing){
+
+    
+        //1. Add current score to active player's score
+        scores[activePlayer]+= currentScore;
+        //scores[1] = scores[1]+currentScore 
+
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        //2.Check if player's score is >= 100
+        if (scores[activePlayer]>=20){
+            //Finish the game
+            playing = false;
+            diceElement.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove ('player--active');
+
+        } else {
+            //Switch to the next player
+            switchPlayer(); 
+
+        }
     }
-})
+
+
+});
