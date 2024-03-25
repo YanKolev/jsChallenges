141888e5c73a2d0dfe1915ca88d14e10bc305181
Yan Kolev <y.kolev11@gmail.com>
2024-03-25 19:59:50 +0200

    Theory addition

---
+++ scriptPartThree.js
@@ -0,0 +1,5 @@
+/* Javascript is a high-level prototype-based object-oriented
+multi-paradigm interpreted or just-in-time compiled
+dynamic single-threaded, garbage-collected programming language
+with first-class functions and non-blocking event-loop conncurency mode.
+*/
