7155dca1bfbfc58172e247d4a8a46575b243e518
Yan Kolev <y.kolev11@gmail.com>
2024-03-26 20:10:32 +0200

    Overview theory

--- scriptPartThree.js
+++ scriptPartThree.js
@@ -3,3 +3,22 @@ multi-paradigm interpreted or just-in-time compiled
 dynamic single-threaded, garbage-collected programming language
 with first-class functions and non-blocking event-loop conncurency mode.
 */
+
+/* Paradigm: An approach and mindset of structuring code, which will direct
+your coding style and technique.
+-procedural, just organizing the code in liniear way
+-OOP- prototype based OOP, 
+almost everyting in JS in an object, without some of the primitive types. 
+
+-prototypal inheritance: 
+
+-functional-
+
+
+-functions are treated as variables (first- class functions)
+
+- Non-blocking event loop:
+what is concurency- how JS engine handles tasks at the same time.
+JS runs in a single thread, if we need a long-running task(we want non-blocking behaviour- we need event loop- that
+takes the running tasks, executes them in the 'background' and puts them back in the main thread once they are finished).
+*/
